acabcf765f203132649443d8522aab1af3696f92
SunPeter <ssd3237649@gmail.com>
2015-03-29 23:07:47 +0800

    css3 3d cube

---
+++ assets/css3cube/js/cube.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+	var stage = document.querySelector("#stage"),
+		cube=document.querySelector("#cube");
+    var X = document.querySelector("#x"),
+        Y = document.querySelector("#y"),
+        Z = document.querySelector("#z");
+
+    var rotateX = document.querySelector("#rotatex"),
+        rotateY = document.querySelector("#rotatey"),
+        rotateZ = document.querySelector("#rotatez");
+
+    var perspectiveX ,perspectiveY ,perspectiveZ;
+
+    var _rotateX,_rotateY,_rotateZ;
+    X.onchange = function() {
+        perspectiveX = this.value;
+        setPerspective(perspectiveX, perspectiveY);
+    }
+    Y.onchange = function() {
+        perspectiveY = this.value;
+        setPerspective(perspectiveX, perspectiveY);
+    }
+    Z.onchange = function() {
+        perspectiveZ = this.value; 
+        stage.style.webkitPerspective= perspectiveZ + "px"
+    }
+
+    rotateX.onchange = function() {
+        _rotateX = this.value;
+        setRotate(_rotateX,_rotateY,_rotateZ);
+    }
+    rotateY.onchange = function() {
+        _rotateY = this.value;
+        setRotate(_rotateX,_rotateY,_rotateZ);
+    }
+    rotateZ.onchange = function() {
+        _rotateZ = this.value;
+        setRotate(_rotateX,_rotateY,_rotateZ);
+    }
+
+}
+function setRotate(x,y,z){
+	x=x||0 
+	y=y||0 
+	z=z||0
+	var rotateText="rotateX("+x+"deg) rotateY("+y+"deg) rotateZ("+z+"deg)"
+	cube.style.webkitTransform=rotateText
+}
+function setPerspective(x, y) {
+	x=x||0,y=y||0
+    stage.style.webkitPerspectiveOrigin = x + "px " + y + "px"
+}
